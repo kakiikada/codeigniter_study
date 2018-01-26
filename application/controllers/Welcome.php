c800315a96481e751dbb8d747cb3cf59ea74c206
kakiikada <kakiikada@gmail.com>
2018-01-27 00:51:18 +0900

    first-commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function view($page = 'home')
+	{
+	        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+	        {
+	                // おっと、そのページはありません！
+	                show_404();
+	        }
+
+	        $data['title'] = ucfirst($page); // 頭文字を大文字に
+
+	        $this->load->view('templates/header', $data);
+	        $this->load->view('pages/'.$page, $data);
+	        $this->load->view('templates/footer', $data);
+	}
+
+}
---
+++ application/views/pages/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>hoge</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <h1>掲示板つくるおー</h1>
+        <ul>
+          <li><a href="#">一つ目</a></li>
+          <li><a href="#">二つ目</a></li>
+          <li><a href="#">三つ目</a></li>
+        </ul>
+      </div>
+    </header>
+  </body>
+</html>
