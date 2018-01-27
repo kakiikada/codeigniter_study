8d0c7c79cb0f586e37523d86918aab602767c6dc
kakiikada <kakiikada@gmail.com>
2018-01-27 09:36:21 +0900

    commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,24 +1,29 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Task extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 	}
 
-	public function view($page = 'home')
-	{
-	        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-	        {
-	                // おっと、そのページはありません！
-	                show_404();
-	        }
+	// public function view($page = 'home')
+	// {
+	//         if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+	//         {
+	//                 // おっと、そのページはありません！
+	//                 show_404();
+	//         }
+  //
+	//         $data['title'] = ucfirst($page); // 頭文字を大文字に
+  //
+	//         $this->load->view('templates/header', $data);
+	//         $this->load->view('pages/'.$page, $data); //about.php
+	//         $this->load->view('templates/footer', $data);
+	// }
 
-	        $data['title'] = ucfirst($page); // 頭文字を大文字に
-
-	        $this->load->view('templates/header', $data);
-	        $this->load->view('pages/'.$page, $data);
-	        $this->load->view('templates/footer', $data);
-	}
+	public function index()[
+		$this->load->mode('task_model');
+		$this->->load->library('');
+	]
 
 }
--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -3,11 +3,44 @@
   <head>
     <meta charset="utf-8">
     <title>hoge</title>
+    <link rel="stylesheet" href="../base.css">
+    <style media="screen">
+    *{
+      padding:0;
+      margin:0;
+    }
+
+    html,body{
+      height:100%;
+      width:100%;
+    }
+
+    .container{
+      max-width:1000px;
+      margin:0 auto;
+    }
+
+    a{
+      text-decoration: none;
+    }
+
+    header{
+
+    }
+
+    header li{
+      float:left;
+      list-style:none;
+      margin:15px;
+    }
+
+
+    </style>
   </head>
   <body>
     <header>
       <div class="container">
-        <h1>掲示板つくるおー</h1>
+        <h1><?php $title ?></h1>
         <ul>
           <li><a href="#">一つ目</a></li>
           <li><a href="#">二つ目</a></li>
